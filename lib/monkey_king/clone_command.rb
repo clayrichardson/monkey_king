f204dbae685ad9c1eda0167d2973f2b8b4422508
Kai Xiang <kxiang@pivotal.io>
2016-02-23 17:22:37 -0800

    allow list of dirs to transform
    Signed-off-by: clay richardson <crichardson@pivotal.io>

--- lib/monkey_king/clone_command.rb
+++ lib/monkey_king/clone_command.rb
@@ -3,35 +3,23 @@ require 'highline/import'
 
 module MonkeyKing
   class CloneCommand < Mothership
-    option :help, :desc => "Show command usage", :alias => "-h",
-      :default => false
-
-    desc "Show Help"
-    input :command, :argument => :optional
-    def help
-      if name = input[:command]
-        if cmd = @@commands[name.gsub("-", "_").to_sym]
-          Mothership::Help.command_help(cmd)
-        else
-          unknown_command(name)
-        end
-      else
-        Mothership::Help.basic_help(@@commands, @@global)
-      end
-    end
 
     desc "Clone the repo and replace secret and env annotation"
-    input(:repo) { ask("Input the github repo of a existing deployment") }
+    input :repo, :argument => true
+    input :dir, :argument => :splat
     def clone
       repo = input[:repo]
+      sub_dir = input[:dir]
+
       directory = repo.split('/').last.split('.').first
       parser = MonkeyKing::Parser.new
 
       raise 'Failed to clone' unless system("git clone #{repo} #{directory}")
       deployment_yaml_files = []
 
-      deployment_yaml_files += Dir.glob("#{directory}/deployments/*.yml")
-      deployment_yaml_files += Dir.glob("#{directory}/bosh-init/*.yml")
+      sub_dir.each do |d|
+        deployment_yaml_files += Dir.glob("#{directory}/#{d}/*.yml")
+      end
 
       deployment_yaml_files.each do |file|
         puts "Transforming #{file}..."
